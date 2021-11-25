706657b0efc330fd8bc45d2151b54f66369f56a3
Tevqoon <jure.smolar@gmail.com>
2021-11-25 02:50:47 +0100

    stuff

--- navadna_iteracija.m
+++ navadna_iteracija.m
@@ -1,8 +1,19 @@
-function [x, X, k] = navadna_iteracija(f, pribl, eps, koraki)
+function [x, X, k] = navadna_iteracija(f, pribl, eps, koraki, target)
+
+switch nargin
+    case 3
+        koraki = eps;
+        target = inf;
+        eps = -inf;
+    
+end
+
     X = pribl;
     for i = 2 : koraki + 1
         X(i) = f(X(i-1));
-        if abs(X(i) - X(i-1)) < eps
+        if abs(X(i) - X(i-1)) < eps 
+            break;
+        elseif abs(X(i) - target) <  eps
             break;
         end
     end
--- tangentna2.m
+++ tangentna2.m
@@ -1,4 +1,4 @@
-function [x, X, k] = tangentna2(f, df, ddf, x0, eps, koraki)
+function [x, X, k] = tangentna2(f, df, ddf, x0, eps, koraki, target)
     g = @(x) x - f(x) / df(x) - (ddf(x) * (f(x))^2) / (2 * (df(x))^3);
-    [x, X, k] = navadna_iteracija(g, x0, eps, koraki);
+    [x, X, k] = navadna_iteracija(g, x0, eps, koraki, target);
 end
